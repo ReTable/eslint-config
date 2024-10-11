71583a3b56f41a9190c6b16640586aa89589ed06
Alexey Plutalov <demiazz.py@gmail.com>
2024-10-11 19:53:42 +0300

    feat: move factories to namespace

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,1 @@
-export { javascript } from './javascript';
-export { react } from './react';
-export { testingLibrary } from './testingLibrary';
-export { typescript } from './typescript';
-export { vitest } from './vitest';
+export * from './factories';
