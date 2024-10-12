607d166dccd8103fe48f6422c65f13543338628e
Alexey Plutalov <demiazz.py@gmail.com>
2024-10-12 16:55:24 +0300

    build: configure `prettier`

---
+++ .prettierrc.cjs
@@ -0,0 +1,18 @@
+const base = require('@tabula/prettier-config');
+
+module.exports = {
+  ...base,
+
+  importOrder: [
+    '^node:.*$',
+
+    '^eslint$',
+    '^@eslint/',
+
+    '<THIRD_PARTY_MODULES>',
+
+    '^~/common',
+    '^~/helpers',
+    '^~/types',
+  ],
+};
