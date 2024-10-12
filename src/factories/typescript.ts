91b8eb5fa0539ecbd4fcd0a4646631eb45bcf863
Alexey Plutalov <demiazz.py@gmail.com>
2024-10-12 23:24:52 +0300

    feat: enable typed rules support in `typescript` factory

--- src/factories/typescript.ts
+++ src/factories/typescript.ts
@@ -1,4 +1,4 @@
-import typescriptPlugin from 'typescript-eslint';
+import typescriptPlugin, { ConfigWithExtends } from 'typescript-eslint';
 
 import { javascript as recommended } from '~/common/javascript';
 import { language } from '~/common/language';
@@ -9,6 +9,18 @@ import { user } from '~/common/user';
 import { buildConfigs } from '~/helpers';
 
 import type { Config, ECMAVersion, Files, Globals, Ignores, Rules, SourceType } from '~/types';
+
+type ParserOptions = NonNullable<
+  NonNullable<ConfigWithExtends['languageOptions']>['parserOptions']
+>;
+
+type Project = ParserOptions['project'];
+
+type ProjectService = ParserOptions['projectService'];
+
+type ProjectFolderIgnoreList = ParserOptions['projectFolderIgnoreList'];
+
+type TsConfigRootDir = ParserOptions['tsconfigRootDir'];
 
 type Options = {
   name: string;
@@ -24,6 +36,11 @@ type Options = {
   sourceType?: SourceType;
 
   rules?: Rules;
+
+  project?: Project;
+  projectService?: ProjectService;
+  projectFolderIgnoreList?: ProjectFolderIgnoreList;
+  tsconfigRootDir?: TsConfigRootDir;
 };
 
 export function typescript({
@@ -32,14 +49,44 @@ export function typescript({
   globals,
   ignores,
   name,
+  project,
+  projectFolderIgnoreList,
+  projectService,
   rules,
   sourceType,
+  tsconfigRootDir,
 }: Options): Array<Config> {
+  const parserOptions: ParserOptions = {};
+
+  let isTyped = false;
+
+  if (project != null) {
+    parserOptions.project = project;
+
+    if (tsconfigRootDir != null) {
+      parserOptions.tsconfigRootDir = tsconfigRootDir;
+    }
+
+    if (projectFolderIgnoreList != null) {
+      parserOptions.projectFolderIgnoreList = projectFolderIgnoreList;
+    }
+
+    isTyped = true;
+  } else if (projectService != null) {
+    parserOptions.projectService = projectService;
+
+    isTyped = true;
+  }
+
+  const [strict, stylistic] = isTyped
+    ? [typescriptPlugin.configs.strictTypeChecked, typescriptPlugin.configs.stylisticTypeChecked]
+    : [typescriptPlugin.configs.strict, typescriptPlugin.configs.stylistic];
+
   return buildConfigs({ name, files, ignores }, [
-    language({ ecmaVersion, globals, sourceType }),
+    language({ ecmaVersion, globals, sourceType, parserOptions }),
     recommended,
-    ...(typescriptPlugin.configs.strict as Array<Config>),
-    ...(typescriptPlugin.configs.stylistic as Array<Config>),
+    ...(strict as Array<Config>),
+    ...(stylistic as Array<Config>),
     unicorn,
     user(rules),
     prettier,
