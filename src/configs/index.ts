662d0290e4d3a341634d122ffb6779bd43a6212d
Alexey Plutalov <demiazz.py@gmail.com>
2024-10-13 21:20:56 +0300

    feat: add `testingLibrary` config

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,4 +1,5 @@
 export { eslint } from './eslint';
 export { jsxA11y } from './jsxA11y';
 export { reactHooks } from './reactHooks';
+export { testingLibrary } from './testingLibrary';
 export { unicorn } from './unicorn';
